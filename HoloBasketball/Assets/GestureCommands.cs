5706fb5af84c8f97436907f65324b48a5c0c1954
Harry <harrywye@outlook.com>
2016-04-24 22:41:36 -0700

    Initial import

---
+++ HoloBasketball/Assets/GestureCommands.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR.WSA.Input;
+
+public class GestureCommands : MonoBehaviour
+{
+    private GestureRecognizer recognizer;
+
+    public Rigidbody prefab;
+    public float speed = 10.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        recognizer = new GestureRecognizer();
+        recognizer.SetRecognizableGestures(GestureSettings.Tap);
+        recognizer.TappedEvent += Recognizer_TappedEvent;
+        recognizer.StartCapturingGestures();
+    }
+
+    private void Recognizer_TappedEvent(InteractionSourceKind source, int tapCount, Ray headRay)
+    {
+        ThrowNewBall();
+    }
+
+    private void ThrowNewBall()
+    {
+        var ball = (Rigidbody)Instantiate(prefab, transform.position, transform.rotation);
+        ball.velocity = (transform.forward + transform.up * 0.8f) * speed;
+    }
+
+
+    public void OnDestroy()
+    {
+        if (recognizer != null) {
+            recognizer.TappedEvent -= Recognizer_TappedEvent;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,35 @@
+# Holo Basketball
+
+_Sample HoloLens app (adapted from "Basketball 3D" game)._
+
+This is an example Unity game from a Build 2016 session:
+
+* [Building a 3D Game with Unity and Visual Studio in 30 Minutes](https://channel9.msdn.com/Events/Build/2016/T607)
+
+The Hoop asset was imported from this page:
+
+* [Hoop 3D Model](http://archive3d.net/?a=download&id=efb427dd)
+
+
+The game has been modified to run on HoloLens.
+It supports two input modes, voice and gesture.
+* You can shoot balls by voice phrases, "Shoot" or "Throw", and
+* by "tap" gesture.
+
+
+I wrote a simple blog post regarding this sample game:
+
+
+* [Learning Unity3D in One Day](http://www.harrysmemo.com/post/143100753957/learning-unity3d-in-one-day)
+
+
+### How to Get Started
+
+Open the game folder, HoloBasketball, in Unity, and build it to create a Visual Studio solution.
+Build the app in the visual studio, and run the game in HoloLens device or emulator.
+
+
+## License
+
+MIT © [Harry Y](https://github.com/harrywye/holobasketball)
+
